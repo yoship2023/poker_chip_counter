94c2cf1cdd32b5f7c478dfce772a755b7cc0a4bf
koike <yoship2006@yahoo.co.jp>
2024-01-10 12:01:54 +0900

    初期コミット

--- app.py
+++ app.py
@@ -5,25 +5,32 @@ from zoneinfo import ZoneInfo
 def poker_chip_counter():
     st.title("チップかぞえチャオ")
 
+    col_01, col_02 = st.columns(2)
+    col_11, col_12 = st.columns(2)
+    col_21, col_22 = st.columns(2)
+    col_31, col_32 = st.columns(2)
+
     # チップの数とBBの入力フィールド
-    bb_value = st.number_input("1BBの点数", value=200, step=100)
-    black_chips = st.number_input("100点（⚫️黒いチップ）の数", value=0)
-    purple_chips = st.number_input("500点（🟣紫色チップ）の数", value=0)
-    blue_chips = st.number_input("1,000点（🔵青いチップ）の数", value=0)
-    yellow_chips = st.number_input("5,000点（🟡黄色チップ）の数", value=0)
-    red_chips = st.number_input("25,000点（🔴赤いチップ）の数", value=0)
-    white_chips = st.number_input("100,000点（⚪️赤いチップ）の数", value=0)
-    light_purple_chips = st.number_input("1,000,000点（薄紫チップ）の数", value=0)
+    black_chips = col_01.number_input("100点（⚫️黒いチップ）の数", value=0)
+    purple_chips = col_11.number_input("500点（🟣紫色チップ）の数", value=0)
+    blue_chips = col_21.number_input("1,000点（🔵青いチップ）の数", value=0)
+    yellow_chips = col_31.number_input("5,000点（🟡黄色チップ）の数", value=0)
+    red_chips = col_02.number_input("25,000点（🔴赤いチップ）の数", value=0)
+    white_chips = col_12.number_input("100,000点（⚪️赤いチップ）の数", value=0)
+    light_purple_chips = col_22.number_input("1,000,000点（薄紫チップ）の数", value=0)
+    bb_value = col_32.number_input("1BBの点数", value=200, step=100)
+
+    col_41, col_42 = st.columns(2)
 
     # 計算ボタン
-    if st.button("計算"):
+    if col_41.button("計算"):
         # 合計計算
         total_chips = black_chips * 100 + purple_chips * 500 \
             + blue_chips * 1000 + yellow_chips * 5000 \
             + red_chips * 25000 + white_chips * 100000 + light_purple_chips * 1000000
         # BB表記への変換
         total_bb = total_chips / bb_value
-        st.subheader(f"合計点数： {total_chips:,} 点 ({total_bb:,.2f} BB)")
+        col_42.subheader(f"合計点数： {total_chips:,} 点 ({total_bb:,.2f} BB)")
 
     # 現在時刻の表示
     current_time = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")

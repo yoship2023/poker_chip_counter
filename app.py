e356d61c65903843196687cba3f048e5fe9ff94d
koike <yoship2006@yahoo.co.jp>
2024-01-10 09:45:07 +0900

    初期コミット

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def poker_chip_counter():
     st.title("チップかぞえチャオ")
@@ -12,20 +13,22 @@ def poker_chip_counter():
     yellow_chips = st.number_input("5000点（🟡黄色のチップ）の数", value=0)
     red_chips = st.number_input("25000点（🔴赤いチップ）の数", value=0)
     white_chips = st.number_input("100000点（⚪️赤いチップ）の数", value=0)
+    light_purple_chips = st.number_input("1000000点（⚪️赤いチップ）の数", value=0)
 
     # 計算ボタン
     if st.button("計算"):
         # 合計計算
-        total_chips = black_chips * 100 + purple_chips * 500 + blue_chips * 1000 \
-            + yellow_chips * 5000 + red_chips * 25000 + white_chips * 100000
+        total_chips = black_chips * 100 + purple_chips * 500 \
+            + blue_chips * 1000 + yellow_chips * 5000 \
+            + red_chips * 25000 + white_chips * 100000 + light_purple_chips * 1000000
         # BB表記への変換
         total_bb = total_chips / bb_value
         st.write(f"合計チップ数: {total_chips} チップ ({total_bb} BB)")
 
     # 現在時刻の表示
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
     # メモ欄
-    memo_title = "メモ 現在時刻: " + current_time
+    memo_title = "メモ 現在時刻（JST）: " + current_time
     memo = st.text_area(memo_title, "")
 
 if __name__ == "__main__":

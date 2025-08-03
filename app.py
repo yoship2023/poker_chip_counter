5b3a10aed723884920d4cc8f61f0edb3cb32ca69
MotoKoikeKeiba <yoship2006@gmail.com>
2025-08-03 11:10:46 +0900

    メモ入力保持機能

--- app.py
+++ app.py
@@ -58,8 +58,7 @@ def poker_chip_counter():
         col_98, col_99 = st.columns([1, 6])
 
         if selected_item == 'JOPT用':
-            st.session_state["chips_01_cnt"] = col_01.number_input("100点（⚫️黒いチップ）の数", min_value=0, key="chips_01_cnt")
-            chips_01_cnt = st.session_state["chips_01_cnt"]
+            chips_01_cnt = col_01.number_input("100点（⚫️黒いチップ）の数", min_value=0, key="chips_01_cnt")
             #chips_01_cnt = col_01.number_input("100点（⚫️黒いチップ）の数", min_value=0, value=0)
             chips_02_cnt = col_02.number_input("500点（🟣紫色チップ）の数", min_value=0, value=0)
             chips_11_cnt = col_11.number_input("1,000点（🔵青いチップ）の数", min_value=0, value=0)
